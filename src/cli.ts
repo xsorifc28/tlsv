649a7de2cedad9832dbbd2e82137e97545fbafc6
Léon Becker <lb@space8.me>
2022-01-01 21:45:39 +0100

    Fixed typo in cli script.

--- src/cli.ts
+++ src/cli.ts
@@ -4,16 +4,16 @@ import Validator, {ValidationResults} from './Validator';
 
 const validation: ValidationResults = Validator(fs.readFileSync(process.argv[2]).buffer);
 
-let anyErrorExist = false;
+let anyErrorExists = false;
 for (const key in validation.results) {
   const result = validation.results[key];
   if (result.isValid == false) {
     console.error('VALIDATION ERROR:', result.message);
-    anyErrorExist = true;
+    anyErrorExists = true;
   }
 }
 
-if (!anyErrorExist) {
+if (!anyErrorExists) {
   const durationFormatted = new Date(validation.duration * 1000).toISOString().substr(11, 12);
   const memoryUsage = parseFloat((validation.memoryUsage * 100).toFixed(2))
   console.log(`Found ${validation.frameCount} frames, step time of ${validation.stepTime} ms for a total duration of ${durationFormatted}`);
