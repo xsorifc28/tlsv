c18c195464404754dd9a795f8037bb33fd494340
Léon Becker <lb@space8.me>
2022-01-01 16:10:44 +0100

    Adjusted the cli tool to be compatible with the new error return.

--- src/cli.ts
+++ src/cli.ts
@@ -1,13 +1,19 @@
 #!/usr/bin/env node
 import * as fs from 'fs';
-import Validator, {ValidationResults} from "./Validator";
+import Validator, {ValidationResults} from './Validator';
 
 const validation: ValidationResults = Validator(fs.readFileSync(process.argv[2]).buffer);
 
-if(validation.error) {
-  console.error('VALIDATION ERROR:', validation.error);
-  process.exit(1);
-} else {
+var anyErrorExist = false;
+for (let key in validation.results) {
+  const result = validation.results[key];
+  if (result.isValid == false) {
+    console.error('VALIDATION ERROR:', result.message);
+    anyErrorExist = true;
+  };
+};
+
+if (!anyErrorExist) {
   const durationFormatted = new Date(validation.duration * 1000).toISOString().substr(11, 12);
   const memoryUsage = parseFloat((validation.memoryUsage * 100).toFixed(2))
   console.log(`Found ${validation.frameCount} frames, step time of ${validation.stepTime} ms for a total duration of ${durationFormatted}`);
