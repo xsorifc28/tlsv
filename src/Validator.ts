44f1d15d18d0d0f80ae16802610b01e8a02eedcf
Samed Ozdemir <samed@xsor.io>
2021-12-31 19:30:27 -0500

    feat: update validator to always return all fields, rename durationSecs to duration (in millis)

--- src/Validator.ts
+++ src/Validator.ts
@@ -3,33 +3,46 @@
  * Validation result is considered valid if error field is undefined
  * If the error field is specified, an error message will be built and the validation should be considered invalid
  * Other fields, if available, are returned to aid in building detailed errors or descriptions
+ * @typedef {Object} ValidationResults - creates a new type named 'ValidationResults'
+ * @property {number} [frameCount=0] - number of frames
+ * @property {number} [memoryUsage=0] - memory usage, ranging from 0 to 1 (1 = 100%), commandCount / 681 = memoryUsage
+ * @property {number} [duration=0] - duration in milliseconds
+ * @property {number} [commandCount=0] - number of commands, maximum being 681. commandCount / 681 = memoryUsage
+ * @property {number} [stepTime=0] - duration between frames
+ * @property {(string | undefined)} [error=undefined] - error message, validation invalid if defined, valid if undefined
  */
 export type ValidationResults = {
-  frameCount?: number;
-  memoryUsage?: number;
-  durationSecs?: number;
-  commandCount?: number;
-  stepTime?: number;
+  frameCount: number;
+  memoryUsage: number;
+  duration: number;
+  commandCount: number;
+  stepTime: number;
   error: string | undefined;
 };
 
 /**
  * Validates a FSEQ file per specs defined at https://github.com/teslamotors/light-show
  * @param {(ArrayBuffer|ArrayBufferLike)} data
- * @returns {{error: string}|{frameCount: number, memoryUsage: number, durationSecs: number, commandCount: number, stepTime: number, error: string}}
+ * @returns ValidationResults
  */
 export default (data: ArrayBuffer | ArrayBufferLike): ValidationResults => {
-  if (!data) {
-    return {
-      error: 'Error - file is corrupt or has no data',
-    };
+  const validationResult: ValidationResults = {
+    commandCount: 0,
+    duration: 0,
+    frameCount: 0,
+    memoryUsage: 0,
+    stepTime: 0,
+    error: undefined,
+  }
+
+  if(!data) {
+    validationResult.error = 'An input type of ArrayBuffer or ArrayBufferLike must be provided!';
+    return validationResult;
   }
 
   const MEMORY_LIMIT = 681;
   const arraysEqual = (a: number[], b: number[]) =>
     a.length === b.length && a.every((v, i) => v === b[i]);
-
-  let error;
 
   const magic = String.fromCharCode(...new Uint8Array(data.slice(0, 4)));
 
@@ -39,56 +52,44 @@ export default (data: ArrayBuffer | ArrayBufferLike): ValidationResults => {
   const minor = header.getUint8(6);
   const major = header.getUint8(7);
   const chCount = header.getUint32(10, true);
-  const frameCount = header.getUint32(14, true);
-  const stepTime = header.getUint8(18);
+
+  validationResult.frameCount = header.getUint32(14, true);
+  validationResult.stepTime = header.getUint8(18);
+
   const compressionType = header.getUint8(20);
 
-  if (
-    magic !== 'PSEQ' ||
-    start < 24 ||
-    frameCount < 1 ||
-    stepTime < 15 ||
-    minor !== 0 ||
-    major !== 2
-  ) {
-    return {
-      error: 'Unknown file format, expected FSEQ v2.0',
-    };
+  if(magic !== 'PSEQ' || start < 24 || validationResult.frameCount < 1 || validationResult.stepTime < 15 || minor !== 0 || major !== 2) {
+    validationResult.error = 'Unknown file format, expected FSEQ v2.0';
+    return validationResult;
   }
 
-  if (chCount !== 48) {
-    return {
-      error: `Expected 48 channels, got ${chCount}`,
-    };
+  if(chCount !== 48) {
+    validationResult.error = `Expected 48 channels, got ${chCount}`;
+    return validationResult;
   }
 
-  if (compressionType !== 0) {
-    return {
-      error: 'Expected file format to be V2 Uncompressed',
-    };
+  if(compressionType !== 0) {
+    validationResult.error = 'Expected file format to be V2 Uncompressed';
+    return validationResult;
   }
 
-  const durationSecs = (frameCount * stepTime) / 1000;
-  if (durationSecs > 5 * 60) {
-    error = `Expected total duration to be less than 5 minutes, got ${new Date(
-      durationSecs * 1000
-    )
-      .toISOString()
-      .substr(11, 12)}`;
+  validationResult.duration = (validationResult.frameCount * validationResult.stepTime);
+  if(validationResult.duration > 5 * 60 * 1000) {
+    const durationStr = new Date(validationResult.duration).toISOString().substr(11, 12);
+    validationResult.error = `Expected total duration to be less than 5 minutes, got ${durationStr}`;
   }
 
   let prevLight: number[] = [];
   let prevRamp: number[] = [];
   let prevClosure1: number[] = [];
   let prevClosure2: number[] = [];
-  let commandCount = 0;
   let pos = start;
 
   const LIGHT_BUFFER_LEN = 30;
   const CLOSURE_BUFFER_LEN = 16;
   const GAP = 2;
 
-  for (let i = 0; i < frameCount; i++) {
+  for (let i = 0; i < validationResult.frameCount; i++) {
     const lights = new Uint8Array(data.slice(pos, pos + LIGHT_BUFFER_LEN));
     pos += LIGHT_BUFFER_LEN;
 
@@ -107,47 +108,40 @@ export default (data: ArrayBuffer | ArrayBufferLike): ValidationResults => {
       closures.map((b) => Math.floor(Math.floor(b / 32) + 1) / 2)
     );
 
-    if (!arraysEqual(light_state, prevLight)) {
+    if(!arraysEqual(light_state, prevLight)) {
       prevLight = light_state;
-      commandCount++;
+      validationResult.commandCount++;
     }
 
-    if (!arraysEqual(ramp_state, prevRamp)) {
+    if(!arraysEqual(ramp_state, prevRamp)) {
       prevRamp = ramp_state;
-      commandCount++;
+      validationResult.commandCount++;
     }
 
-    if (!arraysEqual(closure_state.slice(0, 10), prevClosure1)) {
+    if(!arraysEqual(closure_state.slice(0, 10), prevClosure1)) {
       prevClosure1 = closure_state.slice(0, 10);
-      commandCount++;
+      validationResult.commandCount++;
     }
 
-    if (!arraysEqual(closure_state.slice(10), prevClosure2)) {
+    if(!arraysEqual(closure_state.slice(10), prevClosure2)) {
       prevClosure2 = closure_state.slice(10);
-      commandCount++;
+      validationResult.commandCount++;
     }
 
     pos += GAP;
   }
 
-  const memoryUsage = commandCount / MEMORY_LIMIT;
+  validationResult.memoryUsage = validationResult.commandCount / MEMORY_LIMIT;
 
-  if (memoryUsage > 1) {
-    const memoryUsageFormatted = parseFloat((memoryUsage * 100).toFixed(2));
-    const memError = `Used ${memoryUsageFormatted}% of available memory! Sequence uses ${commandCount} commands, but the maximum allowed is ${MEMORY_LIMIT}!`;
-    if(error) {
-      error += ', ' + memError;
+  if(validationResult.memoryUsage > 1) {
+    const memoryUsageFormatted = parseFloat((validationResult.memoryUsage * 100).toFixed(2));
+    const memError = `Used ${memoryUsageFormatted}% of available memory! Sequence uses ${validationResult.commandCount} commands, but the maximum allowed is ${MEMORY_LIMIT}!`;
+    if(validationResult.error) {
+      validationResult.error += ', ' + memError;
     } else {
-      error = memError
+      validationResult.error = memError
     }
   }
 
-  return {
-    frameCount,
-    stepTime,
-    durationSecs,
-    memoryUsage,
-    commandCount,
-    error,
-  };
+  return validationResult;
 };
--- test/Validator.test.ts
+++ test/Validator.test.ts
@@ -18,7 +18,7 @@ describe('Validator', () => {
       expect(Object.keys(validationResult).length).toBe(6);
       expect(validationResult.frameCount).toBe(2247);
       expect(validationResult.stepTime).toBe(20);
-      expect(validationResult.durationSecs).toBe(44.94);
+      expect(validationResult.duration).toBe(44940);
       expect(validationResult.memoryUsage).toBeGreaterThan(0.16);
       expect(validationResult.memoryUsage).toBeLessThan(0.17);
       expect(validationResult.commandCount).toBe(112);
@@ -31,32 +31,56 @@ describe('Validator', () => {
       it('should return error (no data)', () => {
         // @ts-ignore
         const validationResult: ValidationResults = Validator();
-        expect(validationResult.error).toBe('Error - file is corrupt or has no data');
-        expect(Object.keys(validationResult).length).toBe(1);
+        expect(validationResult).toEqual({
+          error: 'An input type of ArrayBuffer or ArrayBufferLike must be provided!',
+          commandCount: 0,
+          duration: 0,
+          frameCount: 0,
+          memoryUsage: 0,
+          stepTime: 0,
+        });
       });
 
       it('should return error (unknown \'magic\' format)', () => {
         const buffer = readFile('sampleFiles/lightshow_valid.fseq');
         buffer[0] = 79;
         const validationResult: ValidationResults = Validator(buffer.buffer);
-        expect(validationResult.error).toBe('Unknown file format, expected FSEQ v2.0');
-        expect(Object.keys(validationResult).length).toBe(1);
+        expect(validationResult).toEqual({
+          error: 'Unknown file format, expected FSEQ v2.0',
+          commandCount: 0,
+          duration: 0,
+          frameCount: 2247,
+          memoryUsage: 0,
+          stepTime: 20,
+        });
       });
 
       it('should return error (not 48 channels)', () => {
         const buffer = readFile('sampleFiles/lightshow_valid.fseq');
         buffer[11] = 79;
         const validationResult: ValidationResults = Validator(buffer.buffer);
-        expect(validationResult.error).toBe('Expected 48 channels, got 20272');
-        expect(Object.keys(validationResult).length).toBe(1);
+        expect(validationResult).toEqual({
+          error: 'Expected 48 channels, got 20272',
+          commandCount: 0,
+          duration: 0,
+          frameCount: 2247,
+          memoryUsage: 0,
+          stepTime: 20,
+        });
       });
 
       it('should return error (compressionType=1)', () => {
         const buffer = readFile('sampleFiles/lightshow_valid.fseq');
         buffer[20] = 3;
         const validationResult: ValidationResults = Validator(buffer.buffer);
-        expect(validationResult.error).toBe('Expected file format to be V2 Uncompressed');
-        expect(Object.keys(validationResult).length).toBe(1);
+        expect(validationResult).toEqual({
+          error: 'Expected file format to be V2 Uncompressed',
+          commandCount: 0,
+          duration: 0,
+          frameCount: 2247,
+          memoryUsage: 0,
+          stepTime: 20,
+        });
       });
 
       it('should return error (duration > 5m)', () => {
@@ -64,13 +88,14 @@ describe('Validator', () => {
         buffer[18] = 255;
         const validationResult: ValidationResults = Validator(buffer.buffer);
         expect(validationResult.error).toBe('Expected total duration to be less than 5 minutes, got 00:09:32.985');
-        expect(Object.keys(validationResult).length).toBe(6);
-        expect(validationResult.frameCount).toBe(2247);
-        expect(validationResult.stepTime).toBe(255);
-        expect(validationResult.durationSecs).toBe(572.985);
-        expect(validationResult.memoryUsage).toBeGreaterThan(0.16);
-        expect(validationResult.memoryUsage).toBeLessThan(0.17);
-        expect(validationResult.commandCount).toBe(112);
+        expect(validationResult).toEqual({
+          error: 'Expected total duration to be less than 5 minutes, got 00:09:32.985',
+          commandCount: 112,
+          duration: 572985,
+          frameCount: 2247,
+          memoryUsage: 0.1644640234948605,
+          stepTime: 255,
+        });
       });
     });
 
@@ -80,7 +105,7 @@ describe('Validator', () => {
         expect(Object.keys(validationResult).length).toBe(6);
         expect(validationResult.frameCount).toBe(4310);
         expect(validationResult.stepTime).toBe(20);
-        expect(validationResult.durationSecs).toBe(86.2);
+        expect(validationResult.duration).toBe(86200);
         expect(validationResult.memoryUsage).toBeGreaterThan(1.11);
         expect(validationResult.memoryUsage).toBeLessThan(1.12);
         expect(validationResult.commandCount).toBe(756);
@@ -92,7 +117,7 @@ describe('Validator', () => {
         expect(Object.keys(validationResult).length).toBe(6);
         expect(validationResult.frameCount).toBe(3093);
         expect(validationResult.stepTime).toBe(50);
-        expect(validationResult.durationSecs).toBe(154.65);
+        expect(validationResult.duration).toBe(154650);
         expect(validationResult.memoryUsage).toBeGreaterThan(1);
         expect(validationResult.memoryUsage).toBeLessThan(1.7420);
         expect(validationResult.commandCount).toBe(1186);
@@ -106,7 +131,7 @@ describe('Validator', () => {
         expect(Object.keys(validationResult).length).toBe(6);
         expect(validationResult.frameCount).toBe(3093);
         expect(validationResult.stepTime).toBe(255);
-        expect(validationResult.durationSecs).toBe(788.715);
+        expect(validationResult.duration).toBe(788715);
         expect(validationResult.memoryUsage).toBeGreaterThan(1.7410);
         expect(validationResult.memoryUsage).toBeLessThan(1.7420);
         expect(validationResult.commandCount).toBe(1186);
