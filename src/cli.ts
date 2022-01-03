b65ef2ea661801c7a67c26c9785cee51abe1b83a
Léon Becker <lb@space8.me>
2022-01-03 22:50:26 +0100

    feat: add error types
    * fix: wrong buffer reading.
    * feat: add eslint curly spacing rule
    * feat: add error types
    Co-authored-by: Samed Ozdemir <samed@xsor.io>

--- src/cli.ts
+++ src/cli.ts
@@ -1,15 +1,18 @@
 #!/usr/bin/env node
 import * as fs from 'fs';
-import Validator, {ValidationResults} from "./Validator";
+import Validator, { ValidationResults, buildErrorMessages } from './Validator';
 
-const validation: ValidationResults = Validator(fs.readFileSync(process.argv[2]).buffer);
+const file = fs.readFileSync(process.argv[2]);
+const fileArrayBuffer = file.buffer.slice(file.byteOffset, file.byteOffset + file.byteLength);
+const validation: ValidationResults = Validator(fileArrayBuffer);
 
-if(validation.error) {
-  console.error('VALIDATION ERROR:', validation.error);
-  process.exit(1);
+if (validation.errors.length > 0) {
+  buildErrorMessages(validation).forEach(message => {
+		console.error('VALIDATION ERROR:', message);
+	});
 } else {
   const durationFormatted = new Date(validation.duration * 1000).toISOString().substr(11, 12);
-  const memoryUsage = parseFloat((validation.memoryUsage * 100).toFixed(2))
-  console.log(`Found ${validation.frameCount} frames, step time of ${validation.stepTime} ms for a total duration of ${durationFormatted}`);
-  console.log(`Used ${memoryUsage}% of the available memory`);
+  const memoryUsage = parseFloat((validation.memoryUsage * 100).toFixed(2));
+  console.info(`Found ${validation.frameCount} frames, step time of ${validation.stepTime} ms for a total duration of ${durationFormatted}`);
+  console.info(`Used ${memoryUsage}% of the available memory`);
 }
